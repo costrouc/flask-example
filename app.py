8d6b529f5757f71d0a9a4055ac35c021c2a5c4a2
{{ personal_name }} <{{ personal_email }}>
2016-03-20 22:06:39 -0400

    Added stencil of users and user view

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+users = [
+    {
+        'name': 'Chris Ostrouchov',
+        'img_url': '/static/img/chris.jpg',
+        'email': 'chris.ostrouchov@gmail.com'
+    }, { 
+        'name': 'Marci Wetsand',
+        'img_url': '/static/img/maci.jpg',
+        'email': 'maciwhitson@gmail.com'
+    }, {
+        'name': 'Adam Sizemore',
+        'img_url': '/static/img/adam.jpg',
+        'email': 'adam@gmail.com'
+    }, {
+        'name': 'Nikki Hashemian',
+        'img_url': '/static/img/nikki.jpg',
+        'email': 'nikki@gmail.com'
+    }
+]
+
 @app.route('/')
-def index():
-    return "Hello World!"
+def index_view():
+    return render_template('home.html')
+
+@app.route('/users')
+def users_view():
+    return render_template('users.html', users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
